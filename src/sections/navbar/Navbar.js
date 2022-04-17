b42a37a50fa8bd70c4372ceb0292b7155967cb04
kalikayiCode <sunilkalikayiofficial@gmail.com>
2022-04-17 20:46:51 +0530

    navbar is commited

--- src/sections/navbar/Navbar.js
+++ src/sections/navbar/Navbar.js
@@ -1,7 +1,21 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+// logo is imported here ..............!
+import Logo from "../../assets/icons/logo.svg";
+
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="nav-bar">
+      <div className="nav-bar-content">
+        {" "}
+        <img src={Logo} alt="logo of the tamplate"  />
+        <ul className="nav-items keep-me-center">
+          <li className="nav-item">Location</li>
+          <li className="nav-item">Blogs</li>
+          <li className="nav-item">Testimonials</li>
+          <li className="nav-item">Contact </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
